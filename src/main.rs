616440dd164beff81af4f802a7fdb672e8a74876
Bjorn Tipling <bjorn@ambientchill.com>
2018-10-30 13:04:59 -0700

    Run for a time.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use std::time::{
+    Duration,
+    SystemTime,
+};
+
 fn main() {
-    println!("Hello, world!");
+    println!("Starting up!");
+    let mut done = false;
+    let mut duration = Duration::new(0, 0);
+    let start = SystemTime::now();
+    let wait_time = 1; // In seconds.
+    while !done {
+        let now = SystemTime::now();
+        match now.duration_since(start) {
+            Err(e) => {
+                println!("error getting time: {:?}", e);
+                done = true;
+            }
+            Ok(v) => {
+                duration = v;
+                if v.as_secs() >= wait_time {
+                    done = true;
+                }
+            }
+        }
+    }
+
+    println!("All done! Program ran for {:?} seconds", duration.as_secs());
 }
